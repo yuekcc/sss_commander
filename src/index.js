6e83e7279241544dcafb84525874fabaab93db4c
yuekcc <yuekcc@qq.com>
2023-05-15 23:53:28 +0800

    支持绑定 app 的静态文件

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { App } from '@tinyhttp/app';
 import sirv from 'sirv';
+import { resolve, join } from 'node:path';
 
 import { appList } from './app_list';
 
@@ -8,12 +9,27 @@ app.use('/public', sirv('public', { dev: true })).get('/', (_, res) => {
   res.redirect('/public/index.html');
 });
 
+app.use('/metadata', (_, res) => {
+  res.send(
+    appList.map(it => {
+      return {
+        name: it.name,
+        web: it.web,
+      };
+    }),
+  );
+});
+
 appList.forEach(config => {
   console.log('install app: %s', config.name);
-  config.apiRoutes?.forEach(api => {
+  config.apis?.forEach(api => {
     console.log('add api: %s %s', api.method, api.path);
     app[api.method](api.path, api.handle);
   });
+
+  const assetsDir = resolve(join(process.cwd(), config.web));
+  console.log('add web: %s', assetsDir);
+  app.use(config.web, sirv(assetsDir));
 });
 
 app.listen(3000, () => {
