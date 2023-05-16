5d3f446f83265e61fed91e63837475778162f79c
yuekcc <yuekcc@qq.com>
2023-05-16 23:35:02 +0800

    使用外置的 vue

--- build.sh
+++ build.sh
@@ -2,10 +2,12 @@
 
 set -ex
 
+REPO_ROOT="$(pwd)"
+
 pnpm run build:web
 pnpm run build:server
 
-REPO_ROOT="$(pwd)"
+cp ${REPO_ROOT}/node_modules/vue/dist/vue.esm-browser.prod.js public/
 
 build_app() {
     node ${REPO_ROOT}/tools/build-app.mjs
--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,18 @@ export default defineConfig({
   base: process.env.NODE_ENV === 'development' ? '/' : '/public/',
   build: {
     outDir: './public',
+    minify: false,
+    rollupOptions: {
+      external: ['vue'],
+    },
   },
   plugins: [vue()],
   server: {
     proxy: {
       '/api': {
+        target: 'http://127.0.0.1:3000',
+      },
+      '/metadata': {
         target: 'http://127.0.0.1:3000',
       },
       '^/apps/.+/api': {
